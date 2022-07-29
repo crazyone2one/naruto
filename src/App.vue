ddd20e0f811fab1167beae28f3cdd4a467831cf2
jingll.li <the2ndindec@hotmail.com>
2022-07-29 10:44:55 +0800

    feat: :sparkles: element国际化

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useApp } from '@/store/modules/app'
+
+// 配置element中英文
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import en from 'element-plus/es/locale/lang/en'
+
+const store = useApp()
+const locale = computed(() => {
+  if (store && store.language === 'zh-cn') {
+    return zhCn
+  }
+  return en
+})
+</script>
 
 <template>
-  <router-view />
+  <el-config-provider :locale="locale">
+    <router-view />
+  </el-config-provider>
 </template>
 
 <style scoped></style>
